3a782efdbbcb03df7dc06deac00140ea3b9da8ab
nikita-kotliar <knr200710@gmail.com>
2026-01-17 20:26:35 +0200

    done

--- src/book.ts
+++ src/book.ts
@@ -1,26 +1,59 @@
-// TODO: імпортуй потрібні типи з ./types
-// import type { BookId, Genre, LoanStatus } from "./types";
+import type { BookId, Genre, LoanStatus } from "./types";
+
+type BookConstructorOptions = {
+  id: BookId;
+  title: string;
+  author: string;
+  year: number;
+  genre: Genre;
+};
 
 export class Book {
-  // TODO: додай типи до властивостей
-  id;
-  title;
-  author;
-  year;
-  genre;
+  id: BookId;
+  title: string;
+  author: string;
+  year: number;
+  genre: Genre;
 
-  status;
-  borrowedBy;
+  private status: LoanStatus = "available";
+  private borrowedBy: string | null = null;
 
-  // TODO: реалізуй конструктор з параметром opts
-  constructor(opts) {}
+  constructor(opts: BookConstructorOptions) {
+    this.id = opts.id;
+    this.title = opts.title;
+    this.author = opts.author;
+    this.year = opts.year;
+    this.genre = opts.genre;
+  }
 
-  // TODO: методи відповідно до ТЗ
-  getStatus() {}
+  getStatus(): LoanStatus {
+    return this.status;
+  }
 
-  markBorrowed(personName) {}
+  markBorrowed(personName: string): void {
+    if (this.status === "borrowed") {
+      throw new Error(`Already borrowed by ${this.borrowedBy}`);
+    }
 
-  markReturned() {}
+    this.status = "borrowed";
+    this.borrowedBy = personName;
+  }
 
-  getInfo() {}
+  markReturned(): void {
+    if (this.status === "available") {
+      throw new Error("Already available");
+    }
+
+    this.status = "available";
+    this.borrowedBy = null;
+  }
+
+  getInfo(): string {
+    const main = `${this.title} — ${this.author} (${this.year}), ${this.genre}`;
+    if (this.status === "available") {
+      return `${main} [Available]`;
+    }
+
+    return `${main} [Borrowed by ${this.borrowedBy}]`;
+  }
 }
